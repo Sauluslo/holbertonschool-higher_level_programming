c4a0f5ec87ab3d7020c1584009d65958c2a32bf0
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-04-29 05:13:54 +0000

    fix pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,6 +10,6 @@ if count == 0:
     print("{}{} is {} and is 0".format(last_of, number, count))
 elif count < 6 and count != 0:
     print("{}{} is {} and is less than 6 and not 0"
-        .format(last_of, number, count))
+          .format(last_of, number, count))
 else:
     print("{}{} is {} and is greater than 5".format(last_of, number, count))
