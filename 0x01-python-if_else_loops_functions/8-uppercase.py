ecc2265a1aeb3b0cd5d3af9231d6030573269258
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-04-29 04:48:49 +0000

    fix pep8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for count in str:
         if ord(count) >= 97 and ord(count) <= 122:
-           count = chr(ord(count) - 32)
+            count = chr(ord(count) - 32)
         print("{}".format(count), end="")
     print()
