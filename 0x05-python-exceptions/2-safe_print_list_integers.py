5ce3ac0529b91b3305370789fc7c438f43913a35
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-05-18 21:23:27 +0000

    function that prints the first x elements of a list and only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for items in range(x):
+    for elements in range(x):
         try:
-            print("{:d}".format(my_list[items]), end="")
+            print("{:d}".format(my_list[elements]), end="")
             count += 1
         except (TypeError, ValueError):
             pass
