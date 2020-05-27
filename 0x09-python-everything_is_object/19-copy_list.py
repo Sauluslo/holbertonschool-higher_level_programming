3f0f8b3bfb345bae8149e91436c857b2b1c9ef1a
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-05-27 03:39:01 +0000

    new project of holberton about 0x09. Python - Everything is object

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"function def copy_list(l): that returns a copy of a list."
+
+
+def copy_list(l):
+    """ The input list can contain any type of objects
+        Your file should be maximum 3-line long (no documentation needed)
+        You are not allowed to import any module
+    """
+    return l.copy()
