096769965e10bca3934f2c787102085414b78b85
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-06-02 20:00:54 +0000

    class Mylist that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """ inherits from list
+        Print list in order.
+    """
+    def print_sorted(self):
+        print(sorted(self))
