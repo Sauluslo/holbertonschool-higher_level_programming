96fd2883b9ebb22148150212b6b85b9b2a439822
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-06-03 04:50:03 +0000

    class Square that inherits from Rectangle (9-rectangle.py)

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" class Square that inherits from Rectangle (9-rectangle.py)
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Instantiation with size: def __init__(self, size):
+        size must be private. No getter or setter
+        size must be a positive integer, validated by integer_validator
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self, size):
+        return self.__size * self.__size
