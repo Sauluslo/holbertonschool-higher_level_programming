a7939eedc5ece2f2ec6f3aeb838b3388c6e8b229
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-06-03 05:27:40 +0000

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,6 +8,9 @@
 
 
 def add_integer(a, b=98):
+    """ Methotd or function
+        that adds 2 integers.
+    """
     if type(a) is int or type(a) is float:
         if type(b) is int or type(b) is float:
             return int(a) + int(b)
