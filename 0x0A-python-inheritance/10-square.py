d8b3cdfa3b285fe842d431fbfd1457f3f5813dc3
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-06-03 04:53:59 +0000

    class Square that inherits from Rectangle (9-rectangle.py)

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -13,6 +13,6 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
 
-    def area(self, size):
+    def area(self):
         area = self.__size * self.__size
         return area
