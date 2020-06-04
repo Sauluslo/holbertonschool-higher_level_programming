46c0548d00fc70bb30fcc9405d4e65f70f7ef049
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-06-04 00:01:27 +0000

    new project

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 0-read_file.py
+"""
+
+
+def read_file(filename=""):
+    """ Function that reads a text file 
+        (UTF8) and prints it to stdout:
+    """
+    with open(filename, 'r') as file:
+        read_data = file.read()
+        print(read_data)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This is a new project of Holberton School about 0x0B-python-input_output
