de2095e979a167a51e6b20e6a2fe15357bf71111
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-06-04 02:41:43 +0000

    function that writes a string to a text file (UTF8) and returns the number of characters written

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 3-write_file.py
+"""
+
+
+def write_file(filename="", text=""):
+    """ Function that writes a string to a text file (UTF8)
+        and returns the number of characters written:
+    """
+    with open(filename, 'w', encoding="utf-8") as file:
+        return len(text)
