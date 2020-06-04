3054292d61c599f4036e4e1bf59a6160b3b07e13
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-06-04 02:45:40 +0000

    fix style pep8

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -8,5 +8,5 @@ def write_file(filename="", text=""):
         and returns the number of characters written:
     """
     with open(filename, 'w', encoding="utf-8") as file:
-        file.write()
+        file.write(text)
         return len(text)
