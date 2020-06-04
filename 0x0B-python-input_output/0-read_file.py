c4c1be7a0e977f9faaeaa209d7034d6c8df5805d
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-06-04 00:03:34 +0000

    fix style Pep8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,7 @@
 
 
 def read_file(filename=""):
-    """ Function that reads a text file 
+    """ Function that reads a text file
         (UTF8) and prints it to stdout:
     """
     with open(filename, 'r') as file:
