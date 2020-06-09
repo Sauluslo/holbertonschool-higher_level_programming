92b91385cd461ce440de58f1271ffc880b144b52
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-06-09 21:35:04 +0000

    import json and fix pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+""" Class Base
 """
-"""
+import json
+
 
 class Base:
-    """
+    """ Declare Attribute of the class
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """ Init of the id
+            in the class base
+        """
         if id is not None:
             self.id = id
         else:
