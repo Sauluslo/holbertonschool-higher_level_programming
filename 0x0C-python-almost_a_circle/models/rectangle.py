9b3ce1f40aa0558dc204f99961c0bc1eec088bbd
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-06-09 21:58:47 +0000

    fix pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,7 +13,7 @@ class Rectangle(Base):
         """
 
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
