e175baa661068f6a6275dd817098371f12ffce0a
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-07-14 04:57:47 +0000

    all tasks

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- 12. Cheating is bad
+-- A Script that updates the score of Bob to 10 in the table second_table.
+-- You are not allowed to use Bob’s id value, only the name field
+-- The database name will be passed as an argument of the mysql command
+
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,8 @@
+-- First add
+-- Script that inserts a new row in the table first_table
+-- (database hbtn_0c_0) in your MySQL server.
+-- New row:
+-- id = 89
+-- name = Holberton School
+-- The database name will be passed as an argument of the mysql command
+INSERT INTO first_table VALUES (89, 'Holberton School');
