4cbe6e0d1ec24bf3163b23ed7abdfc4a84e0e5c4
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-07-15 04:51:45 +0000

    fix identified by hbtn2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,5 +6,5 @@
 -- (4) If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT SELECT ON hbtn_0d_2.*
-TO user_0d_2@localhost IDENTIFIED BY 'user_0d_pwd';
+TO user_0d_2@localhost IDENTIFIED BY 'user_0d_02_pwd';
 FLUSH PRIVILEGES;
