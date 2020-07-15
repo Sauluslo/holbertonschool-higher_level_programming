c8b48736d4b617f94155dad5e8a7b9b9373bd998
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-07-15 04:31:09 +0000

    script that creates the MySQL server user user_0d_1.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,6 @@
 -- (1) user_0d_1 should have all privileges on your MySQL server
 -- (2) The user_0d_1 password should be set to user_0d_1_pwd
 -- (3) If the user user_0d_1 already exists, your script should not fail
-
+GRANT ALL PRIVILEGES ON * . *
+TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
