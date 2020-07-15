c69a3f2987facf1fb856f9b75d325b95a109ffca
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-07-15 05:22:12 +0000

    comedyyy script that lists all Comedy shows in the database hbtn_0d_tvshows.

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -11,4 +11,9 @@
 -- (5) You can use only one SELECT statement
 -- (6) The database name will be passed
 -- as an argument of the mysql command
-
+SELECT title FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
