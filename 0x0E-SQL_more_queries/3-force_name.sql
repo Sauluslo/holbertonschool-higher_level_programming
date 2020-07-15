a65cb2fa324a4b4ca4badad8abed34e4567c5dcd
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-07-15 04:39:54 +0000

    task 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -5,4 +5,4 @@
 -- name VARCHAR(256) can’t be null
 -- (2) The database name will be passed as an argument of the mysql command
 -- (3) If the table force_name already exists, your script should not fail
-
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
