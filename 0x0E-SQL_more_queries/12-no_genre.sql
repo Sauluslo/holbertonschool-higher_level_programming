95c5c59bca05069311af82ddb5dadcdf8f9cd267
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-07-15 05:09:34 +0000

    script that lists all shows contained in hbtn_0d_tvshows without a genre linked

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -10,4 +10,9 @@
 -- (3) You can use only one SELECT statement
 -- (4) The database name will be passed as an
 -- argument of the mysql command
-
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+RIGHT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
