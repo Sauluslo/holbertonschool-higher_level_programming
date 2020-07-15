53b28297850a5ee10ecee37886fd0e456a87a18e
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-07-15 04:49:58 +0000

    script that creates the database hbtn_0d_usa and the table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,4 +6,9 @@
 -- name VARCHAR(256) can’t be null
 -- (2) If the database hbtn_0d_usa already exists, your script should not fail
 -- (3) If the table states already exists, your script should not fail
-
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(256)
+);
