934176da561f7bd312a44a9a6eac9fcdb4881ebd
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-07-15 04:59:13 +0000

    script that lists all the cities of California that can be found in the database hbtn_0d_usa

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,4 +4,4 @@
 -- (2) Results must be sorted in ascending order by cities.id
 -- (3) You are not allowed to use the JOIN keyword
 -- (4) The database name will be passed as an argument of the mysql command
-
+SELECT id, name FROM cities WHERE state_id = 1 ORDER BY id;
