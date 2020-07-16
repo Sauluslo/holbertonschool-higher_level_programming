cf93765e71a74eb424b469e8a6ff02df75eda4e6
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-07-16 22:28:12 +0000

    error the name, i am retard :V

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -17,4 +17,4 @@ SELECT name AS 'genre', COUNT(*) AS 'number_of_shows'
 FROM tv_genres, tv_show_genres
 WHERE tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY number_shows DESC;
+ORDER BY number_of_shows DESC;
