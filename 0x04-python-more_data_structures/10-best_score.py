047df8c3b1326b73948909db6a6c509908e57275
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-07-19 05:36:59 +0000

    add function that returns a key with the biggest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A Function that returns a key
+    with the biggest integer value.
+"""
+
+
+def best_score(a_dictionary):
+    """ (1) You can assume that all values are only integers.
+        (2) If no score found, return None.
+        (3) You can assume all students have a different score.
+        (4) You are not allowed to import any module.
+    """
+    if a_dictionary is not None:
+        return max(a_dictionary, key=a_dictionary.get)
+    else:
+        return None
